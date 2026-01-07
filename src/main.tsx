4353f3ab04ddd795410826e30509cc097e924ffa
antongrits <antongric558@gmail.com>
2026-01-07 15:04:09 +0300

    Clean up repository: remove variant B files and setup scripts

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// Используем AApp-beta для работы без префикса /a
-import { AApp } from './components/a/AApp-beta'
+import { AApp } from './components/a/AApp'
 import './index.css'
 
-// Entry point для базового прототипа (FitLife_beta)
-// По умолчанию открывается вариант A без префикса /a
+// Entry point for FitLife Beta - basic prototype
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AApp />
   </React.StrictMode>,
 )
-
